375a79afb9a07b2f726db04f57574a4e1a0c6914
BradarsGang <noemail@mail.com>
2026-03-25 07:32:24 +0000

    vendorsetup: Remove FOX_VANILLA_BUILD and fix FOX_VERSION
    - Remove FOX_VANILLA_BUILD=1; contradicts FOX_ENABLE_APP_MANAGER=1
      and strips out most OrangeFox features unnecessarily
    - Fix FOX_VERSION from 34 (Android API level) to R11.1
      which is the correct OrangeFox version format

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,6 @@ add_lunch_combo fox_mgvi_64_64only_armv82-user
 
 # OrangeFox specific exports
 export FOX_VIRTUAL_AB_DEVICE=1
-export FOX_VANILLA_BUILD=1
 export FOX_ENABLE_APP_MANAGER=1
 export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
@@ -18,5 +17,5 @@ export FOX_USE_XZ_UTILS=1
 export FOX_USE_ZSTD_BINARY=1
 export FOX_USE_NANO_EDITOR=1
 export FOX_DELETE_AROMAFM=1
-export FOX_VERSION=34
+export FOX_VERSION=R11.1
 export FOX_MAINTAINER_PATCH_VERSION=$(date +"%Y%m%d")
